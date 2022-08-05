343637bceb693869608c65e0467095564a5db409
Robert <lober3464@gmail.com>
2022-08-05 10:21:23 -0300

    feat: adding the props for the cart

--- react-entrega-s1-hamburgueria-da-kenzie-robert663/src/components/Cart/Cart.jsx
+++ react-entrega-s1-hamburgueria-da-kenzie-robert663/src/components/Cart/Cart.jsx
@@ -1,10 +1,10 @@
 import '../../CSS/Cart/Cart.css';
 import CartProd from '../CartProd/CartProd';
-const Cart = ({list}) => {
+const Cart = ({list, removerItem}) => {
     return(
         <ul className = 'Cart'>
            {list.map((produtos, index) => 
-             <CartProd key = {index}>
+             <CartProd key = {index} onClick={() => removerItem(produtos.id)}>
                   {produtos}
              </CartProd>
   )}
